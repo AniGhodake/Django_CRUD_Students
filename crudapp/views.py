93ef81ef2a83e1d6c4e0c9f2795801614b443d69
Aniket_Ghodake <ghodakeaniket27@gmail.com>
2025-10-02 15:28:34 +0530

    First Commit

---
+++ crudapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Student
+
+def index(request):
+    students = Student.objects.all()
+    context = {"students":students}
+    return render(request, "index.html",context = context)
